49bd1128b4b3044dbaf05b639ab211b56451df40
Zach Deibert <zach@zach-pc-debian>
2016-05-17 14:26:31 -0500

    Added shell support to generated launchers

--- generate.sh
+++ generate.sh
@@ -8,8 +8,14 @@ echo -n "Comment:        "
 read comment
 echo -n "Icon:           "
 read icon
+echo -ne "Default Shell:  bash\rDefault Shell:  "
+read shell
 
-echo "Saving to $HOME/Desktop/$name.desktop"
+if [ "a$shell" == "a" ]; then
+	shell="bash"
+fi
+
+echo "Saving to $HOME/Desktop/$name.desktop and $HOME/Desktop/$name - $shell.desktop"
 cat <<EOF > "$HOME/Desktop/$name.desktop"
 [Desktop Entry]
 Version=1.0
@@ -21,5 +27,16 @@ Terminal=false
 Type=Application
 Categories=Application;
 EOF
-chmod +x "$HOME/Desktop/$name.desktop"
+cat <<EOF > "$HOME/Desktop/$name - $shell.desktop"
+[Desktop Entry]
+Version=1.0
+Name=$name - $shell
+Comment=$comment ($shell shell)
+Exec=gnome-terminal -e "sh -c 'docker ps | grep $container || docker start $container; docker exec -i -t $container $shell'"
+Icon=$icon
+Terminal=false
+Type=Application
+Categories=Application;
+EOF
+chmod +x "$HOME/Desktop/$name.desktop" "$HOME/Desktop/$name - $shell.desktop"
 echo "Done!"
